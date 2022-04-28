ecd25f26cb8d4e454de502094a72d8131d2fda4e
vs-muralidhar <vivin.murali@gmail.com>
2022-04-28 16:54:48 -0700

    MaximumSubarray

---
+++ MaximumSubarray.java
@@ -0,0 +1,23 @@
+public class MaximumSubarray {
+
+    public static int maxSubArray(int[] nums) {
+        // Initialize our variables using the first element.
+        int currentSubarray = nums[0];
+        int maxSubarray = nums[0];
+
+        // Start with the 2nd element since we already used the first one.
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            // If current_subarray is negative, throw it away. Otherwise, keep adding to it.
+            currentSubarray = Math.max(num, currentSubarray + num);
+            maxSubarray = Math.max(maxSubarray, currentSubarray);
+        }
+
+        return maxSubarray;
+    }
+
+    public static void main(String[] args) {
+        int[] input = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println("Maximum Subarray sum is " + maxSubArray(input));
+    }
+}
