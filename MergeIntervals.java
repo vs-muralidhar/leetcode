bec5a8acfb839f257c955ec9f46a4e871b31baf5
vs-muralidhar <vivin.murali@gmail.com>
2022-04-29 12:59:45 -0700

    MergeIntervals

---
+++ MergeIntervals.java
@@ -0,0 +1,21 @@
+// Problem Statement : https://leetcode.com/problems/merge-intervals/
+
+public class MergeIntervals {
+
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        LinkedList<int[]> result = new LinkedList<>();
+
+        for (int[] interval : intervals) {
+            if (result.isEmpty() || result.getLast()[1] < interval[0]) {
+                result.add(interval);
+            } else {
+                result.getLast()[1] = Math.max(result.getLast()[1], interval[1]);
+            }
+
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+
+}
