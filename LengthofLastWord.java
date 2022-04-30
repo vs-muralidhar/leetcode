23e6cca2f26c1cd4a6b6523672cbdd5f331858af
vs-muralidhar <vivin.murali@gmail.com>
2022-04-30 12:30:07 -0700

    LengthofLastWord

---
+++ LengthofLastWord.java
@@ -0,0 +1,21 @@
+// Problem Statement : https://leetcode.com/problems/length-of-last-word/
+
+public class LengthofLastWord {
+
+	public int lengthOfLastWord(String s) {
+        int p = s.length(), length = 0;
+        while (p > 0) {
+            p--;
+            // we're in the middle of the last word
+            if (s.charAt(p) != ' ') {
+                length++;
+            }
+            // here is the end of last word
+            else if (length > 0) {
+                return length;
+            }
+        }
+        return length;
+  }
+
+}
