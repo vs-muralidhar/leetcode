73ab2d68077bb8a1bdbff404016c9ef68b2d02f2
vs-muralidhar <vivin.murali@gmail.com>
2022-05-12 17:29:14 -0700

    ConvertSortedArraytoBinarySearchTree

---
+++ ConvertSortedArraytoBinarySearchTree.java
@@ -0,0 +1,21 @@
+public class ConvertSortedArraytoBinarySearchTree {
+
+		int[] nums;
+    
+    public TreeNode helper(int left, int right) {
+        if (left > right) return null;
+        
+        int p = (left + right)/2;
+        
+        TreeNode root = new TreeNode(nums[p]);
+        root.left = helper(left, p-1);
+        root.right = helper(p+1, right);
+        return root;
+    }
+    
+    public TreeNode sortedArrayToBST(int[] nums) {
+        this.nums = nums;
+        return helper(0, nums.length-1);
+    }
+
+}
