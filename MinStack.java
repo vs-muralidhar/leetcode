d486eaa7763f940d18346cf8a7a6649ea459970d
vs-muralidhar <vivin.murali@gmail.com>
2022-05-21 12:52:21 -0700

    MinStack

---
+++ MinStack.java
@@ -0,0 +1,30 @@
+class MinStack {
+    
+    private Stack<int[]> stack = new Stack<>();
+
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        if (stack.isEmpty()) {
+            stack.push(new int[]{val, val});
+            return;
+        }
+        int currMin = stack.peek()[1];
+        stack.push(new int[]{val, Math.min(val, currMin)});
+    }
+    
+    public void pop() {
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek()[0];
+    }
+    
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
+
